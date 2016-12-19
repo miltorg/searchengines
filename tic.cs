9573a386b1020317b7025e0079d0d2f7ac7184b6
miltorg <miltorg@yandex.ru>
2016-12-19 02:43:06 +0200

    Add files via upload

---
+++ tic.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Net;
+
+
+class Example
+{
+    static void Main()
+    {
+     string input=new WebClient().DownloadString("http://bar-navig.yandex.ru/u?ver=2&url=http://prohorov-andrej.ru&show=1");
+ //Console.WriteLine (input);
+
+        string pattern = @"(value=....)";
+        Regex regex = new Regex(pattern);
+
+        // Достигаем того же результата что и в предыдущем примере,
+        // используя метод Regex.Matches() возвращающий MatchCollection
+        foreach (Match match in regex.Matches(input))
+        {
+            Console.WriteLine(match.Groups[1].Value);
+        }
+    }
+}
